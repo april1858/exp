a5b3932a1d696b6101b1e01640d58189adba2bfa
user <user@astra>
2024-01-26 08:51:16 +0300

    cold

--- main.go
+++ main.go
@@ -8,19 +8,23 @@ import (
 
 func main() {
 	var b strings.Builder
-	s := "a4bc12d5e"
+	s := "d\n5abc"
 	for i := 0; i < len(s); i++ {
-		if s[i] <= 57 {
+		if s[i] == 10 {
+			b.WriteString(string(s[i]))
+			fmt.Println("sd ", b.String())
+		}
+		if s[i] <= 57 && s[i] != 10 {
 			if i == 0 {
 				fmt.Println("error")
 				break
 			}
 			if i < len(s)-1 && s[i+1] <= 57 {
-				fmt.Println("error")
+				fmt.Println("error2")
 				break
 			}
 		}
-		if i < len(s)-1 && s[i+1] <= 57 {
+		if i < len(s)-2 && s[i+1] <= 57 && s[i+2] > 57 {
 			repeat, _ := strconv.Atoi(string(s[i+1]))
 			b.WriteString(strings.Repeat(string(s[i]), repeat))
 			i++
