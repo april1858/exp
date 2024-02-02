264f692c883198d6ac3030cd8d738df48b5a17b1
user <user@astra>
2024-02-02 08:44:11 +0300

    unocode

--- main.go
+++ main.go
@@ -3,59 +3,33 @@ package main
 import (
 	"errors"
 	"fmt"
-	"strings"
 )
 
-var InvalidInput = errors.New("invalid input")
+var NotFound error = errors.New("Не найдено")
 
-func Unpack(strIn string) (string, error) {
-	strOut := ""
-	var buff string
-	fmt.Printf("%s ", string(rune(10)))
-	for _, c := range strIn {
-		if c == 10 {
-			c = 92
+func binarySearchs(s []int, item int) (int, error) {
+	low := 0
+	high := len(s) - 1
+	mid := -1
+	for low <= high {
+		mid = (high + low) / 2
+		if s[mid] == item {
+			return mid, nil
 		}
-		fmt.Println("cc - ", c)
-		if c <= 57 {
-			if buff == "" {
-				return "", InvalidInput
-			} else {
-				strOut += strings.Repeat(buff, int(c-'0'))
-				buff = ""
-			}
-		}
-		if c > 57 {
-
-			if buff == "" {
-				if c == 92 {
-					buff = string(c) + "n"
-				} else {
-					buff = string(c)
-				}
-
-			} else {
-				if c == 92 {
-					strOut += buff
-					buff = string(c) + "n"
-				} else {
-					strOut += buff
-					buff = string(c)
-				}
-
-			}
+		if s[mid] < item {
+			low = mid + 1
+		} else {
+			high = mid - 1
 		}
 	}
-	if buff != "" {
-		strOut += string(buff)
-	}
-	return strOut, nil
+	return -1, NotFound
 }
 
 func main() {
-	s, err := Unpack("a\n2b3nc")
+	s := []int{1, 4, 6, 7, 8}
+	answer, err := binarySearchs(s, 1)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(s)
+	fmt.Println(answer)
 }
